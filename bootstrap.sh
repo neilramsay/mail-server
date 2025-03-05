756e98a941b099ffa58205f7a9161bf1e463babf
Neil Ramsay <neil.ramsay@agentnoel.geek.nz>
2025-03-05 23:23:25 +1300

    feat: Add bootstrap for ansible-pull

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+PROJECT=${1:-"mail-server"}
+
+if ! test -d /usr/share/doc/python3-venv; then
+    sudo apt-get update &&
+    sudo apt-get install -y python3-venv
+else
+    sudo true
+fi
+
+if ! test -d ansible; then
+    python3 -m venv ansible &&
+    source ansible/bin/activate &&
+    pip install --require-virtualenv ansible
+else
+    source ansible/bin/activate
+fi
+
+ansible-pull -U https://github.com/neilramsay/${PROJECT}.git
